cbb5f0c9951aa9bef44bded09ce95498db1de71d
Julia <kurdina.00@gmail.com>
2024-05-10 11:23:48 +0300

    test_lab_2

--- llvm/labs/lab2/kurdina_julia/AddPass.cpp
+++ llvm/labs/lab2/kurdina_julia/AddPass.cpp
@@ -3,7 +3,7 @@
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassPlugin.h"
 
