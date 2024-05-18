258486c7c2a0e074712018bfcf86bf51cac9a78d
Julia <kurdina.00@gmail.com>
2024-05-19 01:56:28 +0300

    first test

--- mlir/lib/Transforms/lab4/kurdina_julia/MaxDepthPass.cpp
+++ mlir/lib/Transforms/lab4/kurdina_julia/MaxDepthPass.cpp
@@ -1,7 +1,6 @@
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
 
 using namespace mlir;
 
@@ -14,48 +13,30 @@ public:
   }
 
   void runOnOperation() override {
-
-    getOperation()->walk([&](Operation *op) {
-      if (auto funcOp = dyn_cast<LLVM::LLVMFuncOp>(op)) {
-        int maxDepth = 0;
-        funcOp.walk([&](Block *block) {
-          int curDepth = 0;
-          DepthBlock(block, curDepth, maxDepth);
-        });
+    getOperation().walk([&](Operation *op) {
+      if (auto funcOp = dyn_cast<FuncOp>(op)) {
+        int maxDepth = getMaxDepth(funcOp);
         funcOp->setAttr(
-            "maxDepth",
-            IntegerAttr::get(IntegerType::get(funcOp.getContext(), 32),
-                             maxDepth));
+            "maxNestDepth",
+            IntegerAttr::get(IntegerType::get(getContext(), 32), maxDepth));
       }
-    });
+    });  
   }
 
 private:
-  void DepthBlock(Block *block, int &curDepth, int &maxDepth) {
-    block->walk([&](Operation *op) {
-      auto loopOp = dyn_cast<loop::LoopOp>(op);
-      auto ifOp = dyn_cast<IfOp>(op);
-      // Проверяем, является ли операция условием или циклом
-      if (loopOp != nullptr || ifOp != nullptr) {
-        // Увеличиваем текущую глубину при входе в условие или цикл
-        curDepth++;
-        // Обновляем максимальную глубину, если текущая глубина больше
-        if (curDepth > maxDepth) {
-          maxDepth = curDepth;
+  int getMaxDepth(FuncOp funcOp) {
+    int maxDepth = 1;
+    funcOp.walk([&](Operation *op) {
+      int depth = 1;
+      while (op) {
+        if (auto region = dyn_cast<Region>(op)) {
+          depth++;
         }
+        op = op->getBlock()->getParentOp();
       }
-      auto *nestedBlock = dyn_cast<Block>(op);
-      if (nestedBlock != nullptr) {
-        // Рекурсивно обходим дочерний блок с текущей глубиной и максимальной
-        // глубиной
-        DepthBlock(nestedBlock, curDepth, maxDepth);
-      }
-
-      if (loopOp != nullptr || ifOp != nullptr) {
-        // Уменьшаем текущую глубину при выходе из условия или цикла
-        curDepth--;
-      }
+      maxDepth = std::max(maxDepth, depth);
     });
+    return maxDepth;
   }
 };
 } // namespace
